a7f7be7184b6ddb0f4e118ac7de5529f94b9c402
Sergei <sergei.kachilov@yandex.ru>
2024-10-22 23:26:53 +0500

    Изменен слайдер, добавлены возможность скролла и метки времени

--- DVS/MainWindow.xaml.cs
+++ DVS/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -12,6 +13,7 @@ namespace MediaPlayerApp
         private bool isDragging = false;
         bool isPaused;
         DispatcherTimer timer = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +30,10 @@ namespace MediaPlayerApp
                 timer.IsEnabled = true;
                 timer.Tick += new EventHandler(Timer_Tick);
                 timer.Interval = new TimeSpan(1000);
+                if(!isPaused)
+                {
+                    mediaElement.Play();
+                }
             }
 
         }
@@ -36,7 +42,30 @@ namespace MediaPlayerApp
             if (mediaElement.NaturalDuration.HasTimeSpan)
             {
                 sMusic.Maximum = mediaElement.NaturalDuration.TimeSpan.TotalSeconds;
-                sMusic.Value = mediaElement.Position.TotalSeconds;
+                if (!isDragging)
+                {
+                    sMusic.Value = mediaElement.Position.TotalSeconds;
+                }
+            }
+
+            if (mediaElement != null)
+            {
+                int s = (int)mediaElement.Position.TotalSeconds;
+                int h = s / 3600;
+                int m = (s - (h * 3600)) / 60;
+                s = s - (h * 3600 + m * 60);
+                lStart.Content = String.Format("{0:D}:{1:D2}:{2:D2}", h, m, s);
+
+                s = (int)sMusic.Maximum;
+                h = s / 3600;
+                m = (s - (h * 3600)) / 60;
+                s = s - (h * 3600 + m * 60);
+                lEnd.Content = String.Format("{0:D}:{1:D2}:{2:D2}", h, m, s);
+            }
+            else
+            {
+                lStart.Content = "0:00:00";
+                lEnd.Content = "0:00:00";
             }
         }
         
@@ -51,7 +80,7 @@ namespace MediaPlayerApp
 
         private void sMusic_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-             // Начало перетаскивания
+            
         }
 
         private void sMusic_PreviewMouseUp(object sender, MouseButtonEventArgs e)
@@ -64,7 +93,7 @@ namespace MediaPlayerApp
         }*/
         private void sMusic_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> args)
         {
-   
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -81,6 +110,17 @@ namespace MediaPlayerApp
                 mediaElement.Play();
                 bPause.Content = "Pause";
             }
+        }
+
+        private void sMusic_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            isDragging = true;
+        }
+
+        private void sMusic_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            isDragging = false;
+            mediaElement.Position = TimeSpan.FromSeconds(sMusic.Value);
         }
     }
 }
