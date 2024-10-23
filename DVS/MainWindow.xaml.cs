806bffda5039d6debac1bbb858d1211cc64edaa9
Damir <dhadiulin@gmail.com>
2024-10-23 22:03:54 +0500

    Сделал две кнопки, но чем больше их, тем труднее

--- DVS/MainWindow.xaml.cs
+++ DVS/MainWindow.xaml.cs
@@ -3,16 +3,19 @@ using System;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 namespace MediaPlayerApp
 {
     public partial class MainWindow : Window
     {
-        private bool isDragging = false;
-        bool isPaused = false;
-        DispatcherTimer timer = new DispatcherTimer();
+        private bool isDragging = false; // если нет перемещения ползунка
+        private bool isPaused = false; // если нет паузы
+        private bool isOpen; // если файл открыт
+        private readonly DispatcherTimer timer = new DispatcherTimer();
 
         public MainWindow()
         {
@@ -23,19 +26,25 @@ namespace MediaPlayerApp
         {
             // При открытии файла, он сразу воспроизводится
             isPaused = false;
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Media Files|*.mp3";
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Media Files|*.mp3"
+            };
             if (openFileDialog.ShowDialog() == true)
             {
+                isOpen = true;
                 mediaElement.Source = new System.Uri(openFileDialog.FileName);
                 mediaElement.LoadedBehavior = MediaState.Manual; // Устанавливаем поведение загрузки
                 timer.IsEnabled = true;
-                timer.Interval = TimeSpan.FromSeconds(1);
+                timer.Interval = TimeSpan.FromSeconds(0.0001);
                 timer.Tick += Timer_Tick;
+                
                 if (!isPaused)
                 {
+                    var template = bPause.Template;
+                    var image = (Image)template.FindName("ButtonImage", bPause);
+                    image.Source = new BitmapImage(new Uri("/pause.png", UriKind.Relative));
                     mediaElement.Play();
-                    bPause.Content = "Pause";
                 }
             }
         }
@@ -56,20 +65,12 @@ namespace MediaPlayerApp
 
             if (mediaElement != null)
             {
-                int s = (int)mediaElement.Position.TotalSeconds;
-                int h = s / 3600;
-                int m = (s - (h * 3600)) / 60;
-                s = s - (h * 3600 + m * 60);
                 lStart.Content = mediaElement.Position.ToString(@"hh\:mm\:ss");
-                /*s = (int)sMusic.Maximum;
-                h = s / 3600;
-                m = (s - (h * 3600)) / 60;
-                s = s - (h * 3600 + m * 60);*/
             }
             else
             {
-                lStart.Content = "0:00:00";
-                lEnd.Content = "0:00:00";
+                lStart.Content = "00:00:00";
+                lEnd.Content = "00:00:00";
             }
             
         }
@@ -84,19 +85,21 @@ namespace MediaPlayerApp
         }  
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            var template = bPause.Template;
+            var image = (Image)template.FindName("ButtonImage", bPause);;
             isPaused = !isPaused;
-            if (isPaused)
+            if (isPaused && isOpen)
             {
                 mediaElement.Pause();
-                bPause.Content = "Play";
+                image.Source = new BitmapImage(new Uri("/play.png", UriKind.Relative));
             }
-            else
+            if (!isPaused && isOpen)
             {
                 mediaElement.Play();
-                bPause.Content = "Pause";
+                image.Source = new BitmapImage(new Uri("/pause.png", UriKind.Relative));
             }
         }
-        // полузнок перемещается
+        // ползунок перемещается
         private void sMusic_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
         {
             isDragging = true;
@@ -109,8 +112,10 @@ namespace MediaPlayerApp
         }
         private void MediaElement_MediaEnded(object sender, EventArgs e)
         {
+            var template = bPause.Template;
+            var image = (Image)template.FindName("ButtonImage", bPause);
+            image.Source = new BitmapImage(new Uri("/play.png", UriKind.Relative));
             mediaElement.Stop();
-            bPause.Content = "Play";
             isPaused = true;
             sMusic.Value = 0;
 
