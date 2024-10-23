d5c330d5765c5f2015dd9c9ed9b505473a30367c
Damir <dhadiulin@gmail.com>
2024-10-23 12:52:46 +0500

    Сделал полностью слайдер и поигрался с кнопками и label.

--- DVS/MainWindow.xaml.cs
+++ DVS/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -10,8 +11,9 @@ namespace MediaPlayerApp
     public partial class MainWindow : Window
     {
         private bool isDragging = false;
-        bool isPaused;
+        bool isPaused = false;
         DispatcherTimer timer = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -19,6 +21,8 @@ namespace MediaPlayerApp
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
+            // При открытии файла, он сразу воспроизводится
+            isPaused = false;
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Media Files|*.mp3";
             if (openFileDialog.ShowDialog() == true)
@@ -26,51 +30,61 @@ namespace MediaPlayerApp
                 mediaElement.Source = new System.Uri(openFileDialog.FileName);
                 mediaElement.LoadedBehavior = MediaState.Manual; // Устанавливаем поведение загрузки
                 timer.IsEnabled = true;
-                timer.Tick += new EventHandler(Timer_Tick);
-                timer.Interval = new TimeSpan(1000);
+                timer.Interval = TimeSpan.FromSeconds(1);
+                timer.Tick += Timer_Tick;
+                if (!isPaused)
+                {
+                    mediaElement.Play();
+                    bPause.Content = "Pause";
+                }
             }
-
         }
         private void Timer_Tick(object sender, EventArgs e)
         {
             if (mediaElement.NaturalDuration.HasTimeSpan)
             {
                 sMusic.Maximum = mediaElement.NaturalDuration.TimeSpan.TotalSeconds;
-                sMusic.Value = mediaElement.Position.TotalSeconds;
+                if (!isDragging)
+                {
+                    sMusic.Value = mediaElement.Position.TotalSeconds;
+                }
+                //Оставшиееся время до конца трека
+                TimeSpan remainingTime = mediaElement.NaturalDuration.TimeSpan - mediaElement.Position;
+                // Обновляем текстовое содержимое с оставшимся временем
+                lEnd.Content = remainingTime.ToString(@"hh\:mm\:ss");
             }
-        }
-        
-        /*private void MediaSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (isDragging)
+
+            if (mediaElement != null)
             {
-                // Здесь можно обновить воспроизведение медиа в зависимости от значения слайдера
-                // Например, перемотка медиа на новое время
+                int s = (int)mediaElement.Position.TotalSeconds;
+                int h = s / 3600;
+                int m = (s - (h * 3600)) / 60;
+                s = s - (h * 3600 + m * 60);
+                lStart.Content = mediaElement.Position.ToString(@"hh\:mm\:ss");
+                /*s = (int)sMusic.Maximum;
+                h = s / 3600;
+                m = (s - (h * 3600)) / 60;
+                s = s - (h * 3600 + m * 60);*/
             }
-        }*/
-
-        private void sMusic_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-             // Начало перетаскивания
-        }
-
-        private void sMusic_PreviewMouseUp(object sender, MouseButtonEventArgs e)
-        {
+            else
+            {
+                lStart.Content = "0:00:00";
+                lEnd.Content = "0:00:00";
+            }
             
         }
-        /*private void sMusic_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            ((Slider)sender).SelectionEnd = e.NewValue;
-        }*/
+
+        //Перенос ползунка по точке
         private void sMusic_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> args)
         {
-   
-        }
-
+            if (!isDragging && mediaElement.NaturalDuration.HasTimeSpan)
+            {
+                mediaElement.Position = TimeSpan.FromSeconds(sMusic.Value);
+            }
+        }  
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             isPaused = !isPaused;
-
             if (isPaused)
             {
                 mediaElement.Pause();
@@ -81,6 +95,25 @@ namespace MediaPlayerApp
                 mediaElement.Play();
                 bPause.Content = "Pause";
             }
+        }
+        // полузнок перемещается
+        private void sMusic_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            isDragging = true;
+        }
+        // ползунок не перемещается
+        private void sMusic_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            isDragging = false;
+            mediaElement.Position = TimeSpan.FromSeconds(sMusic.Value);
+        }
+        private void MediaElement_MediaEnded(object sender, EventArgs e)
+        {
+            mediaElement.Stop();
+            bPause.Content = "Play";
+            isPaused = true;
+            sMusic.Value = 0;
+
         }
     }
 }
