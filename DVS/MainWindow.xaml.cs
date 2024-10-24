3b3c4e02ed1de8e1e7434c63732eda97d5ad1224
Sergei <sergei.kachilov@yandex.ru>
2024-10-25 00:02:42 +0500

    Улучшена работа со слайдером

--- DVS/MainWindow.xaml.cs
+++ DVS/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -16,10 +17,14 @@ namespace MediaPlayerApp
         private bool isPaused = false; // если нет паузы
         private bool isOpen; // если файл открыт
         private readonly DispatcherTimer timer = new DispatcherTimer();
+        private bool isSpaceDown = false;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            bPause.Focus();
+            sMusic.Focus();
         }
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
@@ -36,7 +41,7 @@ namespace MediaPlayerApp
                 mediaElement.Source = new System.Uri(openFileDialog.FileName);
                 mediaElement.LoadedBehavior = MediaState.Manual; // Устанавливаем поведение загрузки
                 timer.IsEnabled = true;
-                timer.Interval = TimeSpan.FromSeconds(0.005);
+                timer.Interval = TimeSpan.FromSeconds(0.000001);
                 timer.Tick += Timer_Tick;
                 
                 if (!isPaused)
@@ -70,19 +75,9 @@ namespace MediaPlayerApp
                 lStart.Content = "00:00:00";
                 lEnd.Content = "00:00:00";
             }
-            
         }
 
-        //Перенос ползунка по точке
-        private void sMusic_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> args)
-        {
-            if (!isDragging && mediaElement.NaturalDuration.HasTimeSpan &&
-               (((mediaElement.Position - TimeSpan.FromSeconds(args.NewValue)) > TimeSpan.FromSeconds(0.5)) || (TimeSpan.FromSeconds(args.NewValue) - mediaElement.Position > TimeSpan.FromSeconds(0.5))))
-            {
-                mediaElement.Position = TimeSpan.FromSeconds(args.NewValue);
-            }
-        }  
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Pause()
         {
             isPaused = !isPaused;
             if (isPaused && isOpen)
@@ -95,6 +90,21 @@ namespace MediaPlayerApp
                 mediaElement.Play();
                 PlayImage.Source = new BitmapImage(new Uri("/pause.png", UriKind.Relative));
             }
+        }
+
+        //Перенос ползунка по точке
+        private void sMusic_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> args)
+        {
+            if (!isDragging && mediaElement.NaturalDuration.HasTimeSpan && 
+                ((mediaElement.Position - TimeSpan.FromSeconds(args.NewValue) > TimeSpan.FromSeconds(0.5)) 
+                || (TimeSpan.FromSeconds(args.NewValue) - mediaElement.Position > TimeSpan.FromSeconds(0.5))))
+            {
+                mediaElement.Position = TimeSpan.FromSeconds(sMusic.Value);
+            }
+        }  
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Pause();
         }
         // ползунок перемещается
         private void sMusic_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
@@ -114,6 +124,46 @@ namespace MediaPlayerApp
             isPaused = true;
             sMusic.Value = 0;
 
+        }
+
+        private void sMusic_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void bPause_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                mediaElement.Position -= TimeSpan.FromSeconds(5);
+            }
+
+            if(e.Key == Key.Right)
+            {
+                mediaElement.Position += TimeSpan.FromSeconds(5);
+            }
+        }
+
+        private void sMusic_PreviewDragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void sMusic_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void sMusic_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                var slider = (Slider)sender;
+                Point position = e.GetPosition(slider);
+                double d = 1.0d / slider.ActualWidth * position.X;
+                var p = slider.Maximum * d;
+                slider.Value = p;
+            }
         }
     }
 }
