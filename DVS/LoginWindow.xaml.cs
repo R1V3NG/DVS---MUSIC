09c99e1630aeed0a68c8ea0ea85ec39a13b2e334
Sergei <sergei.kachilov@yandex.ru>
2024-11-06 17:46:15 +0500

    Сделана регистрация и вход пользователей

---
+++ DVS/LoginWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MediaPlayerApp;
+using Microsoft.Data.Sqlite;
+using System.Text.RegularExpressions;
+
+namespace DVS
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        string activeUser;
+        bool isCorrect = false;
+
+        public static string path = @"audioplayer.db";
+
+        public static List<Users> users = new List<Users>();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Err.Visibility = Visibility.Hidden;
+            users.Clear();
+
+            if(CheckTextBox())
+            {
+                DbConnect();
+                CheckData();
+            }
+            else
+            {
+                MessageBox.Show("Некорректный ввод данных!", "Ошибка");
+            }
+        }
+
+        bool CheckTextBox()
+        {
+            Regex regex = new Regex(@"\s");
+            if(regex.IsMatch(tLogin.Text) || regex.IsMatch(tPassword.Text) || tLogin.Text == "" || tPassword.Text == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static void DbConnect()
+        {
+            string sqlExpression = "SELECT login, password FROM users";
+            string connectionString = $"Data Source={path};Mode=ReadWrite";
+
+            using (var conn = new SqliteConnection(connectionString))
+            {
+                conn.Open();
+
+                SqliteCommand command = new SqliteCommand(sqlExpression, conn);
+
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            users.Add(new Users() { login = Convert.ToString(reader.GetValue(0)), password = Convert.ToString(reader.GetValue(1)) });
+                        }
+                    }
+                }
+            }
+
+        }
+
+        void CheckData()
+        {
+            foreach (var user in users)
+            {
+                if (user.login == tLogin.Text && user.password == tPassword.Text)
+                {
+                    activeUser = user.login;
+                    isCorrect = true;
+                }
+            }
+
+            if (isCorrect)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            else
+            {
+                Err.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            RegisterWindow registerWindow = new RegisterWindow();
+            registerWindow.Show();
+        }
+    }
+}
---
+++ DVS/RegisterWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using Microsoft.Data.Sqlite;
+using System.IO;
+
+namespace DVS
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(CheckTextBox())
+            {
+                if (CheckPassword())
+                {
+                    if(CheckLogin())
+                    {
+                        DbConnect();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пользователь с таким именем уже существует", "Ошибка!");
+                    }
+                }
+
+                else
+                {
+                    MessageBox.Show("Пароли не совпадают", "Ошибка!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Логин и/или пароль содержат неразрешенные символы!", "Ошибка!");
+            }
+        }
+        bool CheckTextBox()
+        {
+            Regex logRegex = new Regex("[^a-zA-Z0-9]");
+            Regex passRegex = new Regex(@"\s");
+
+            if(logRegex.IsMatch(tLogin.Text) || passRegex.IsMatch(tPassword.Text) || tLogin.Text == "" || tPassword.Text == "" || tCheckPassword.Text == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        bool CheckPassword()
+        {
+            if(tPassword.Text == tCheckPassword.Text)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        bool CheckLogin()
+        {
+            LoginWindow.users.Clear();
+            LoginWindow.DbConnect();
+
+            bool isCorrect = true;
+
+            foreach(var user in LoginWindow.users)
+            {
+                if(user.login == tLogin.Text)
+                {
+                    isCorrect = false;
+                }
+            }
+            return isCorrect;
+        }
+
+        void DbConnect()
+        {
+            string connectionString = $"Data Source={LoginWindow.path};Mode=ReadWrite";
+            using(var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                SqliteCommand command = new SqliteCommand();
+
+                string commandText = "INSERT INTO users (login, password) VALUES (@login, @password)";
+                command.Connection = connection;
+                command.CommandText = commandText;
+                command.Parameters.AddWithValue("@login", tLogin.Text);
+                command.Parameters.AddWithValue ("@password", tPassword.Text);
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
